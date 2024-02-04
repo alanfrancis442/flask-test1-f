41e84015d0a8f8e4a2cf71b1e90dbb5a9f773ac5
Alan Francis <119670162+alanfrancis442@users.noreply.github.com>
2024-02-04 15:39:16 +0530

    new files added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,53 @@
-import Image from "next/image";
+'use client'
+import 'bootstrap/dist/css/bootstrap.css'
 
 export default function Home() {
   return (
     <>
-    this is the frontend
+    <div className="flex text-white justify-center h-screen items-center bg-slate-600">
+        <form className="w-1/3 h-1/2">
+          <div className="form-group">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+              // onChange={nameHandeler}
+              // value={name}
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+              // value={pass}
+              // onChange={passHandeler}
+            />
+          </div>
+          <div className="form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label">Check me out</label>
+          </div>
+          <button type="submit" className="mx-2 btn btn-primary">
+            Submit
+          </button>
+          <button type="submit" className="mx-2 btn btn-primary">
+            Register
+          </button>
+        </form>
+      </div>
     </>
   );
 }
---
+++ app/user/page.tsx
@@ -0,0 +1,9 @@
+function UserPage() {
+    return ( <>
+    <div>
+        the name of the user is this
+    </div>
+    </> );
+}
+
+export default UserPage;
